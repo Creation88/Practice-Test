e24a05a3daff707bd9191494171f587a86a8940a
Creator88 <creationeightyeight@gmail.com>
2022-01-06 18:13:27 -1000

    Creation

---
+++ main.py
@@ -0,0 +1,192 @@
+# age = " 21"
+# first_name = "Gambium"
+# last_name = "Clark"
+# full_name = first_name +" "+ last_name
+# print ("Your name is "+ str(full_name))
+
+
+# height = 69.420
+# print("Your height is " + str(height) + "cm")
+
+
+# human = False
+# print(type(human))
+# print("Are you a human?: "+str(human))
+
+
+# name = "tony"
+# print (len(name))
+# print(name.find("t"))
+# print(name.capitalize())
+# print(name.lower())
+# print(name.upper())
+# print(name.isalpha())
+# print(name.isdigit())
+# print(name.count("t"))
+# print(name.replace("t","b"))
+# print(name*9999999)
+
+# x = 1
+# y = 2.0
+# z = "3"
+
+# y = int(2.0)
+# x = float(x)
+# z = float(z)
+# y = float(y)
+
+# print("x is a fat "+str(x))
+# print(y)
+# print(z)
+
+# age = input("How old are you?")
+# print("so you are "+(age)+ (" years old."))
+
+# height = float(input("What is your height?"))
+# name = input("What is your name?")
+# age = int(input("How old are you?"))
+# print("Hello " +(name)+ " so you are "+str(age)+" Years old."+" And you are "+str(height)+" feet tall.")
+# import math
+# print(round(3.56))
+# print(math.ceil(3.1))
+# print(math.floor(3.1))
+# print(abs(-9))
+# print(pow(9, 2))
+# print(math.sqrt(81))
+# print(max(9, 3, 91))
+# print(min(9, 81, 203))
+
+# name = "Tony bark"
+
+# first_name = name[:4]
+# last_name = name[5:9]
+# weird_name = name[::2]
+# reversed_name = name[::-1]
+# print (reversed_name)
+# print(first_name)
+# print(last_name)
+
+# website = "http://youtube.com"
+# website2 = "http://google.com"
+
+# slice = slice(7,-4)
+# print(website2[slice])
+# print(website[slice])
+
+
+# favorite_burger = (input("What is your favorite burger?"))
+# if favorite_burger == "Cheeseburger":
+#    print("Cheeseburgers are great!")
+# elif favorite_burger == "blt":
+#    print("You like BLTS!")
+#
+# else: print("You don't like Cheeseburgers!")
+# temp = int(input("What is the temperature?"))
+# if temp >= 0 and temp <= 30:
+#    print("It is nice out, go outside!")
+# elif temp =:
+#    print("The temperature is bad, don't go outside!")
+# elif not(temp >= 110 or temp <= 105):
+#    print("Easter egg!")
+# name = ""
+# while len(name) == 0:
+#    name = input("Enter your name: ")
+# print("Your name is "+name+"! ")
+
+# name = None
+# while not name:
+#   name = input("Enter your name")
+#
+# print("Hello "+name+"!")
+
+# for i in range(10):
+#    print(i+1)
+
+# for i in range(50,100+1,):
+#        print(i)
+
+# for i in "Tony Clark":
+#    print(i)
+# import time
+# from time import sleep
+# for i in range(1, 10):
+#    print(i), sleep(2)
+# print("Congratulations, you won!")
+# import time
+# for seconds in range(10,0,-1):
+#    print(seconds)
+#    time.sleep(0.35)
+# time.sleep(5)
+# print("Congratulations, you won!")
+
+# solidware = ["bowl", "plate", "cup"]
+# silverware = ["spoon", "fork", "knife"]
+
+# for x in solidware:
+#    for y in silverware:
+#        print(x, y)
+# import time
+# rows = ("Tony", "Anthony","Clark")
+# columns = ("Klick", "Klack", "Boom")
+# symbol = input("Enter whatever symbol you please.")
+
+# for i in (rows):
+#    for j in (columns):
+#        print(symbol, end="")
+#        time.sleep(1)
+#    print()
+
+# import time
+
+# column = int(input("Enter your column size here."))
+# row = int(input("Enter your row size here."))
+# symbol = input("Enter a symbol of your choosing here.")
+# for i in range(row):
+#    for j in range(column):
+#        print(symbol, end="")
+#    print()
+
+
+#phone_number = "6618880987"
+
+#for i in phone_number:
+#    if i == "6":
+#        continue
+#    print(i, end="")
+
+#Name = input("What is your name?")
+
+#while True:
+#    if Name == "Trent":
+#        Name = input("What is your name?")
+#    if Name != "Trent":
+#        break
+#print(Name)
+
+
+#Fruit = ("Cherry", "Banana", "Mango", "Avacado", "Cherry", "Mango", "Watermelon", "Knife", "Fork")
+#
+#for i in Fruit:
+#    if i == "Cherry":
+#        continue
+#    elif i == "Mango":
+#        continue
+#    elif i == "Knife":
+#        continue
+#    elif i == "Fork":
+#        continue
+#    else:
+#        print(i+" ", end="")
+
+print ("test")
+
+
+
+
+
+
+
+
+
+
+
